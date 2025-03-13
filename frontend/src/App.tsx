328236ad1797863761cba956f4826e9d9bddacec
adrianyu91 <adrianyu91@gmail.com>
2025-03-13 00:04:15 -0400

    chore: Changed category from a string to a List of strings

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { MantineProvider } from '@mantine/core';
 import NavBar from './components/navbar';
 import Workout from './components/workout';
 import './App.css';
@@ -9,21 +8,19 @@ function App() {
   const items = ["Home", "Workout", "Contact"];
 
   return (
-    <MantineProvider>
-      <Router>
-        <div>
-          <NavBar 
-            brandName="HoopLab" 
-            imageSrcPath={''} 
-            navItems={items}
-          />
-          <Routes>
-            <Route path="/workout" element={<Workout />} />
-            {/* Add other routes here */}
-          </Routes>
-        </div>
-      </Router>
-    </MantineProvider>
+    <Router>
+      <div>
+        <NavBar 
+          brandName="HoopLab" 
+          imageSrcPath={''} 
+          navItems={items}
+        />
+        <Routes>
+          <Route path="/workout" element={<Workout />} />
+          {/* Add other routes here */}
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
