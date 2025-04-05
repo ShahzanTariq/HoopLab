e9425de7b3e34b192fd51d4f839d6807530936b7
ShahzanTariq <shahzantariq@gmail.com>
2025-04-05 15:21:50 -0400

    style: cleaned up comments

--- frontend/src/components/workoutPlanMenu.tsx
+++ frontend/src/components/workoutPlanMenu.tsx
@@ -3,23 +3,21 @@ import { Menu, Button, MenuItem } from '@mantine/core';
 interface WorkoutPlanMenuProps {
   userPlans: any[];
   selectedPlan: any | null;
-  onCreatePlan: () => void;   // Callback for creating a new plan
-  onSelectPlan: (plan: any | null) => void; // Callback for selecting a plan
+  onCreatePlan: () => void; 
+  onSelectPlan: (plan: any | null) => void; 
 }
 
 
 function WorkoutPlanMenu({ userPlans, selectedPlan, onCreatePlan, onSelectPlan }: WorkoutPlanMenuProps) {
-
-
   return (
     <Menu shadow="md" width={200}>
       <Menu.Target>
         <Button>{selectedPlan ? selectedPlan.planName : "Select Plan"}</Button>
       </Menu.Target>
       <Menu.Dropdown>
-        <MenuItem onClick={onCreatePlan}>Create New Plan</MenuItem> {/* Use prop */}
+        <MenuItem onClick={onCreatePlan}>Create New Plan</MenuItem> 
         {userPlans.map((plan) => (
-          <MenuItem key={plan.planID} onClick={() => onSelectPlan(plan)}> {/* Use prop */}
+          <MenuItem key={plan.planID} onClick={() => onSelectPlan(plan)}>
             {plan.planName}
           </MenuItem>
         ))}
